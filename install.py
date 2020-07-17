72980afc5d97ada736d7197d9f0619bb10fdfff1
Gabriel Perko-Engel <gabriel@pop-os.localdomain>
2020-07-17 10:03:59 -0700

    Folder fixing.

--- install.py
+++ install.py
@@ -19,8 +19,12 @@ if len(sys.argv) == 1:
     raise ValueError("No input provided.\nPlease specify either 'link' or 'move'.")
 for theme in themes:
     if sys.argv[1] == "link":
+    	if not os.path.exists(tardir):
+            os.mkdir(tardir)
         link(theme)
     elif sys.argv[1] == "move":
+        if not os.path.exists(tardir):
+            os.mkdir(tardir)
         move(theme)
     else:
         raise ValueError("Invalid input provided.\nPlease specify either 'link' or 'move'.")
