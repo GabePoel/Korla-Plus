d9a2eb77e85dbb747fb8e1d5a77bd82cc948ee30
GabePoel <gabrielpoel@gmail.com>
2020-08-03 12:14:40 -0700

    Fixed install bug.

--- install.py
+++ install.py
@@ -12,14 +12,14 @@ def link(name):
 
 def move(name):
     os.system("mvdir '" + os.path.join(insdir, name) + "' " + tardir)
-    
+
 themes = ["korla-plus", "korla-plus-light", "korla-plus-light-panel", "korla-plus-pgrey"]
 
 if len(sys.argv) == 1:
     raise ValueError("No input provided.\nPlease specify either 'link' or 'move'.")
 for theme in themes:
     if sys.argv[1] == "link":
-    	if not os.path.exists(tardir):
+        if not os.path.exists(tardir):
             os.mkdir(tardir)
         link(theme)
     elif sys.argv[1] == "move":
